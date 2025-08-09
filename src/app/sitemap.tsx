73ee693474a1ab73cf413807711475d90c42585d
Gourav R <75977813+g-gourav-r@users.noreply.github.com>
2025-08-09 16:19:24 +0530

    Update Sitemap

--- src/app/sitemap.tsx
+++ src/app/sitemap.tsx
@@ -10,5 +10,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "yearly",
       priority: 1,
     },
+    {
+      url: `${baseUrl}/services`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
   ];
 }
