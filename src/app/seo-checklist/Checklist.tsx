eb18e8af40111996f6a2390ef3aa29319b0a9213
Gourav Ravichandra <145653975+GouravRavichandra@users.noreply.github.com>
2025-08-18 12:49:33 +0530

    Change the icon

--- src/app/seo-checklist/Checklist.tsx
+++ src/app/seo-checklist/Checklist.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import { CircleSmall } from "lucide-react";
 
 interface ChecklistItem {
   label: string;
@@ -40,18 +41,7 @@ export default function Checklist({ data }: ChecklistProps) {
                   {item.subItems.map((subItem, subIndex) => (
                     <li key={subIndex} className="flex items-start">
                       {/* Checkmark icon for visual appeal */}
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-5 w-5 text-neon-yellow mr-2 flex-shrink-0"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
+                      <CircleSmall className="text-neon-yellow me-2" />
                       {subItem}
                     </li>
                   ))}
